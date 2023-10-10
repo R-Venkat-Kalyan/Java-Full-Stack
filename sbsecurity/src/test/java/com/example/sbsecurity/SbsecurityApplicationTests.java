1d05f8f56af4ad7517c2f4e73029e87e83196897
Reddy Venkat Kalyan <110080245+R-Venkat-Kalyan@users.noreply.github.com>
2023-10-10 19:08:28 +0530

    simple SB security application

---
+++ sbsecurity/src/test/java/com/example/sbsecurity/SbsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
